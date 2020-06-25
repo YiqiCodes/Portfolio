0fa2fb484b1d11a16376a3bda7cde3afb20c4c6f
Yiqi Zhang <>
2020-06-25 11:54:31 -0400

    first commit

---
+++ src/App.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  min-width: 100vw;
+  z-index: 1;
+  background: white;
+  color: black;
+`;
