ea4671626a0a6781e0fb3bfb1eb2119aea112623
Yiqi Zhang <>
2020-06-26 15:27:51 -0400

    functional skeleton done

--- src/Pages/About/About.styles.js
+++ src/Pages/About/About.styles.js
@@ -1,7 +1,63 @@
 import styled from "styled-components";
 
-export const Test = styled.div`
+export const AboutContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background: slategray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #eef2ed;
+`;
+
+export const DetailsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: transparent;
+`;
+export const AboutContentContainer = styled.div`
+  width: 50%;
+  height: 80%;
+`;
+
+export const SummaryContainer = styled.div`
+  background: transparent;
+  padding: 1rem;
+`;
+export const BottomContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 20%;
+  width: 100%;
+`;
+export const ResumeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 40%;
+  padding: 0.5rem;
+`;
+export const SocialsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  height: 100%;
+  width: 60%;
+  padding: 0.5rem;
+`;
+
+export const ProfilePictureContainer = styled.img`
+  width: 8rem;
+  height: 10rem;
+`;
+
+export const Details = styled.div`
+  padding: 1rem;
+`;
+
+export const SocialIconDiv = styled.div``;
+export const SocialImg = styled.img`
+  width: 25px;
+  height: 25px;
 `;
